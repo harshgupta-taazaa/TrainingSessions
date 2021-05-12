952b5c849b4afb958928970fc2a95db0a3bd9f01
harshgupta-taazaa <harsh.gupta@taazaa.com>
2021-05-12 20:17:27 +0530

    EnoughIsEnoughKataSolution

---
+++ Day1/DeleteOccurrences/DeleteOccurrences/DeleteOccurrence.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeleteOccurrences
+{
+    public class DeleteOccurrence
+    {
+        List<int> newArray = new List<int>();
+        public static List<int> newArray1=new List<int>();
+        List<int> newArray2 = new List<int>();
+
+       
+        public List<int> DeleteExtraOccurrence(List<int> arr,int count)
+        {
+            newArray1 = arr;
+            for (int i = 0; i < arr.Count - 1; i++)
+            {
+
+               RemoveExtraElements(newArray1, arr[i], count);
+            }
+            return newArray1;
+        }
+
+        public void RemoveExtraElements(List<int> arr, int Itemname,int Count)
+        {
+
+            if (NoOfTimesItemPresent(arr, Itemname) > Count)
+            {
+                int a = 0;
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    if (arr[i] == Itemname)
+                    {
+                        a += 1;
+                        if (a <=Count)
+                        {
+                            newArray2.Add(arr[i]);
+                        }
+                    }
+                    else
+                    {
+                        newArray2.Add(arr[i]);
+                    }
+                }
+                newArray1.Clear();
+                foreach(int p in newArray2)
+                {
+                    newArray1.Add(p);
+
+                }
+                
+                newArray2.Clear();
+            }
+        }
+        public int NoOfTimesItemPresent(List<int> arr,int item)
+        {
+            int count = 0;
+            foreach(int i in arr)
+            {
+                if (i == item)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Day1/DeleteOccurrences/DeleteOccurrences/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeleteOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> arr = new List<int>() { 1,2,3,2,3,3,2,1 };
+            
+            DeleteOccurrence Manager = new DeleteOccurrence();
+            List<int> Result =Manager.DeleteExtraOccurrence(arr, 2);
+            foreach (int a in Result)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
